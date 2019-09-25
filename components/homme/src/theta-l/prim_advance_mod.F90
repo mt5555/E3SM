ae103bc294ce6531458fc24771941e0d7d547d76
Oksana Guba <oksanaguba@gmail.com>
2019-09-25 07:58:18 -0700

    theta_ref on, dp_ref off

--- components/homme/src/theta-l/prim_advance_mod.F90
+++ components/homme/src/theta-l/prim_advance_mod.F90
@@ -1076,11 +1076,11 @@ contains
      temp(:,:,:)=theta_ref(:,:,:,ie)*dp_ref(:,:,:,ie) 
      call phi_from_eos(hvcoord,elem(ie)%state%phis,&
           temp(:,:,:),dp_ref(:,:,:,ie),phi_ref(:,:,:,ie))
-#if 0
+#if 1
      ! no reference state, for testing
      theta_ref(:,:,:,ie)=0
      phi_ref(:,:,:,ie)=0
-     dp_ref(:,:,:,ie)=0
+     !dp_ref(:,:,:,ie)=0
 #endif
 
      ! convert vtheta_dp -> theta
