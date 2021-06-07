f639da2b38adc02891295757fb39dfc8fa143d99
Mark Taylor <mt@mp3dev.org>
2021-06-07 16:09:01 -0500

    new hv_theta_correction = 3 option
    use original CAM-EUL approximation to Biharmonoic_p operator,
    advance with RK3 instead of forward euler

--- components/homme/src/theta-l/element_state.F90
+++ components/homme/src/theta-l/element_state.F90
@@ -81,6 +81,7 @@ module element_state
     real (kind=real_kind) :: theta_ref(np,np,nlev)
     real (kind=real_kind) :: phi_ref(np,np,nlevp)
     real (kind=real_kind) :: lap_p_wk(np,np,nlev)  
+    real (kind=real_kind) :: biharm_p(np,np,nlev)  
   end type derived_state_t
   
 
